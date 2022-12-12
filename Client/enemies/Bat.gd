d935e7be8f194501e0e0b27693be01ebfe8edfb9
Georgios Roros <groros2@icloud.com>
2022-12-12 11:54:18 -0700

    got enemies spawning on server

---
+++ Client/enemies/Bat.gd
@@ -0,0 +1,5 @@
+extends KinematicBody2D
+
+
+func move_enemy(new_position):
+	position = new_position
--- Client/world/world.gd
+++ Client/world/world.gd
@@ -2,7 +2,9 @@ extends YSort
 
 onready var Player = preload("res://player/player.tscn")
 onready var PlayerTemplate = preload("res://player/playertemplate.tscn")
+onready var enemy_spawn = preload("res://enemies/Bat.tscn")
 onready var players = $Players
+onready var enemies = $Enemies
 
 var last_world_state = 0
 var world_state_buffer = []
@@ -11,6 +13,13 @@ const interpolation_offset = 100
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
+	
+func spawn_new_enemy(enemy_id, enemy_data):
+	var new_enemy = enemy_spawn.instance()
+	new_enemy.position = enemy_data["EnemyLocation"]
+	# TO-DO: add stats here like enemy health
+	new_enemy.name = str(enemy_id)
+	enemies.add_child(new_enemy, true)
 
 func spawn_new_player(player_id, player_position, player_name, flip_h):
 	if not players.has_node(str(player_id)):
@@ -54,11 +63,14 @@ func _physics_process(delta):
 			for player in world_state_buffer[2].keys():
 				if str(player) == "T":
 					continue
+				if str(player) == "Enemies":
+					continue
 				if player == get_tree().get_network_unique_id():
 					continue
 				if not world_state_buffer[1].has(player):
 					continue
 				if players.has_node(str(player)):
+					# TO-DO: add health here like we have in the enemy code below to set up pvp health display
 					var new_position = lerp(world_state_buffer[1][player]["P"], world_state_buffer[2][player]["P"], interpolation_factor)
 					var animation_state = "Idle"
 					if world_state_buffer[2][player].has("A"):
@@ -69,6 +81,15 @@ func _physics_process(delta):
 					players.get_node(str(player)).move_player(new_position, animation_state, flip_h)	
 				else:
 					spawn_new_player(player, world_state_buffer[2][player]["P"], Server.players[int(player)]["player_name"], false)
+			for enemy in world_state_buffer[2]["Enemies"].keys():
+				if not world_state_buffer[1]["Enemies"].has(enemy):
+					continue
+				if enemies.has_node(str(enemy)):
+					var new_position = lerp(world_state_buffer[1]["Enemies"][enemy]["EnemyLocation"], world_state_buffer[2]["Enemies"][enemy]["EnemyLocation"], interpolation_factor)
+					enemies.get_node(str(enemy)).move_enemy(new_position)
+					# enemies.get_node(str(enemy)).health(world_state_buffer[1]["Enemies"][enemy]["EnemyHealth"])
+				else:
+					spawn_new_enemy(enemy, world_state_buffer[2]["Enemies"][enemy])
 		elif render_time > world_state_buffer[1].T:
 			var extrapolation_factor = float(render_time - world_state_buffer[0].T) / float(world_state_buffer[1].T - world_state_buffer[0].T) - 1.00
 			for player in world_state_buffer[1].keys():
